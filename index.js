78a465a1e4c5ae23e673e6fcb39af5b5a51c50bc
scherida rayane <scheridarayane@gmail.com>
2020-08-19 22:32:14 -0300

    Testando aplicação com Node

--- index.js
+++ index.js
@@ -4,10 +4,12 @@
 //Declarar as dependências do arquivo
 //Se for uma past nossa ./(Coloca caminho)
 //Se não só passar ''(aspas simples);
-const express = require('express');
+const express = require("express");
 const app = express();
 
-
+app.get("/", (req, res) => {
+  res.send('<h1>Primeira aplicação com Node.Js</h1>');
+});
 
 //Definir a porta que o servidor vai escutar
 const port = 1234;
